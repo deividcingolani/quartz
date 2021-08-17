e4da3c7647f83abea46255c0033a94aa9097a3e9
Gonzalo Uceda <gon_u@hotmail.com>
2021-08-17 22:55:25 +0200

    fix avg execution time formatting (#175)

--- src/pages/project/jobs/utils/getAVGtime.ts
+++ src/pages/project/jobs/utils/getAVGtime.ts
@@ -1,9 +1,18 @@
-import { format } from 'date-fns';
+import { JobExecutionData } from '../../../../types/jobs';
 
-const getAVGtime = (executions: any[]) => {
-  const avg =
-    executions.reduce((ac, a) => a.duration + ac, 0) / executions.length;
-  return format(new Date(avg), "h'h' mm'm' ss's'");
+const getAVGtime = (executions: JobExecutionData[]): string => {
+  const sum = executions.reduce((ac, a) => a.duration + ac, 0);
+  const avg = sum / executions.length;
+
+  const seconds = Math.floor((avg / 1000) % 60);
+  const minutes = Math.floor((avg / (1000 * 60)) % 60);
+  const hours = Math.floor((avg / (1000 * 60 * 60)) % 24);
+
+  const hours2Digits = hours < 10 ? `0${hours}` : hours;
+  const minutes2Digits = minutes < 10 ? `0${minutes}` : minutes;
+  const seconds2Digits = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${hours2Digits}h ${minutes2Digits}m ${seconds2Digits}s`;
 };
 
 export default getAVGtime;
